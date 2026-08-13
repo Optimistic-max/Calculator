a53cc2acde47c41678d4a3091e341aab9cfe1984
Emmanuel <chukwuemekae066@gmail.com>
2026-08-13 14:23:55 -0400

    Added grid

--- Calculator.py
+++ Calculator.py
@@ -27,9 +27,9 @@ window.resizable(False, False)
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text='0', font = ("Arial", 45),
                       background=color_black,
-                      foreground = color_white)
+                      foreground = color_white, anchor = "e")
 
-label.grid(row=0, column=0)
+label.grid(row=0, column=0,columnspan = column_count, sticky = "we")
 
 for row in range(row_count):
     for column in range(column_count):
@@ -37,10 +37,33 @@ for row in range(row_count):
         button = tkinter.Button(frame, text = value, font =("Arial",30),
                                 width=column_count-1,height=1,
                                 command=lambda value=value: button_clicked(value))
+        if value in top_symbols:
+            button.config(foreground=color_black, background=color_light_gray)
+        elif value in right_symbols:
+            button.config(foreground=color_white, background=color_orange)
+        else:
+            button.config(foreground=color_white, background=color_dark_grey)
+        button.grid(row=row+1, column=column)
         
 frame.pack()
 
-def button_clicked(value):
-    pass
+#A+B, A-B,A*B, A/B
+A = "0"
+operator = None
+B=None
 
+
+def button_clicked(value):
+    global right_symbols, top_symbols, A, B, operator
+#center the window
+window.update() #update window with new size dimensions
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 window.mainloop()
